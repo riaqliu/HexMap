56963213e1df22531d79a586730dd59138638f5f
riaqliu <riaqliu@gmail.com>
2023-02-08 20:56:51 +0800

    Part 2

---
+++ Hexagons/HexCell.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCell : MonoBehaviour
+{
+   public HexCoordinates coordinates;
+   public Color color;
+
+   [SerializeField]
+   HexCell[] neighbors;
+
+   public HexCell GetNeighbor(HexDirection direction)
+   {
+      return neighbors[(int)direction];
+   }
+
+   public void SetNeighbor(HexDirection direction, HexCell cell)
+   {
+      neighbors[(int)direction] = cell;
+      cell.neighbors[(int)direction.Opposite()] = this;
+   }
+}
+
+// reference: https://catlikecoding.com/unity/tutorials/hex-map/part-1/
